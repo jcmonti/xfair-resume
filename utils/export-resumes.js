d230e84678c0a9851d3901768d0b95fc2fdba1a7
Yuhan Luo <yuhanluov@gmail.com>
2017-01-25 18:40:13 -0500

    de-dup

--- utils/export-resumes.js
+++ utils/export-resumes.js
@@ -12,12 +12,21 @@ function recordToFilename(record) {
 }
 
 Record.find({ filled_out: true }).exec(function(err, records) {
+  var hash = {};
+
   if (err)
     throw err;
 
   for (var i = 0; i < records.length; i += 1) {
-    var record = records[i];
-    console.log([record.major_name[0], record.s3_url, recordToFilename(record)].join(','));
+    id = records[i].mit_id;
+    email = records[i].email;
+    createdAt = records[i].createdAt;
+    if (hash[id] === undefined
+     || hash[id].email !== email
+     || hash[id].createdAt < createdAt) {
+      var record = records[i];
+      console.log([record.major_name[0], record.s3_url, recordToFilename(record)].join(','));
+    }
   }
 
   process.exit();
