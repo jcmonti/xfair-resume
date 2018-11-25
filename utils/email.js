1a2dd8dba47f878ac7715c2578dcfb12d23a5382
nhagen <nhagen@mit.edu>
2018-11-24 23:02:56 -0500

    Update email.js

--- utils/email.js
+++ utils/email.js
@@ -6,9 +6,9 @@ if (process.env.SMTP_ENABLE == 'yes') {
     user: process.env.SMTP_USERNAME,
     password: process.env.SMTP_PASSWORD,
     host: process.env.SMTP_HOST,
-    port: 465,
+    port: 587,
     ssl: true,
-    tls: true,
+    tls: true
   });
   module.exports = function(options) {
     var email = options.email;
